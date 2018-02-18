d7f67005283a67b904934ead41d684f8290a3464
maisaa <maisaa.alkhedr@gmail.com>
2018-02-18 12:31:44 +0200

    SinglR chat

---
+++ SigChat/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SigChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+       public void Send(String name, string message)
+        {
+            // Call the addNewMessageToPage method to update clinet
+            Clients.All.addNewMessageToPage(name, message);
+
+        }
+    }
+}
---
+++ SigChat/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SigChat.Startup))]
+namespace SigChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            // Any connection or hub wire up and configrathion should go here
+            app.MapSignalR();
+        }
+    }
+}
